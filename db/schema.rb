359874c75288a5e065ed34e892a7ec62249ee573
Inga Rondel <90716651+rondelinga@users.noreply.github.com>
2026-02-16 17:20:18 +0300

    cwt-42/feat: add pinned to labels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_30_061021) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_16_125946) do
   # These extensions should be enabled to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
@@ -1201,6 +1201,17 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_30_061021) do
     t.index ["name", "account_id"], name: "index_teams_on_name_and_account_id", unique: true
   end
 
+  create_table "user_pinned_labels", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "label_id", null: false
+    t.integer "position", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["label_id"], name: "index_user_pinned_labels_on_label_id"
+    t.index ["user_id", "label_id"], name: "index_user_pinned_labels_on_user_id_and_label_id", unique: true
+    t.index ["user_id"], name: "index_user_pinned_labels_on_user_id"
+  end
+
   create_table "users", id: :serial, force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
@@ -1272,6 +1283,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_30_061021) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "inboxes", "portals"
+  add_foreign_key "user_pinned_labels", "labels"
+  add_foreign_key "user_pinned_labels", "users"
   create_trigger("accounts_after_insert_row_tr", :generated => true, :compatibility => 1).
       on("accounts").
       after(:insert).
